ac2f393fa525835e1dc555a8b69df5e23898c2a0
code-sparsh <sparshsethi15@gmail.com>
2024-07-03 07:01:50 +0530

    refactor: blog component adjusted for image

--- src/components/BlogComponent.js
+++ src/components/BlogComponent.js
@@ -10,19 +10,20 @@ const BlogComponent = ({ blog: blog }) => {
     }
 
     return (
-        <div className=" bg-gray-100 p-10 border shadow-sm flex">
-            <div className="object-cover h-6 w-80">
-                <img src={blog.imageURL} alt="blog image" />
+        <div className="bg-gray-100 p-10 border shadow-sm flex flex-col lg:flex-row">
+            <div className=" h-64 lg:h-44 lg:w-3/6 w-full">
+                <img src={blog.imageURL} alt="blog image" className="h-full w-full object-cover" />
             </div>
-            <div className=" grid">
+            <div className="grid mt-4 lg:mt-0 lg:ml-4">
                 <div className="flex justify-between">
                     <Link to={"/blog/" + blog.id} className="title text-2xl font-bold hover:underline">{blog.title}</Link>
                     <div className="author text-right font-semibold hover:underline">By - {blog.author}</div>
                 </div>
-
                 <div className="mt-4 break-words">{shortData}</div>
             </div>
         </div>
+
+
     )
 }
 
